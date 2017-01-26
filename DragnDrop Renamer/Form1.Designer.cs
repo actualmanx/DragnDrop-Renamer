df8cd67fe1d1ff1024905a89d26e118616a14850
Kieran Hill <dilligaf1979@gmail.com>
2017-01-26 19:39:21 +0000

    added dragNdrop

--- DragnDrop Renamer/Form1.Designer.cs
+++ DragnDrop Renamer/Form1.Designer.cs
@@ -28,21 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Visual Studio 2013 Dark";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(370, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Old File Name";
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.ClientSize = new System.Drawing.Size(698, 431);
+            this.Controls.Add(this.label1);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DragnDrop Renamer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DragnDrop Renamer/Form1.cs
+++ DragnDrop Renamer/Form1.cs
@@ -15,5 +15,46 @@ namespace DragnDrop_Renamer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   // Enable drag and drop for this form
+            // (this can also be applied to any controls)
+            // AllowDrop = true;
+
+            // Add event handlers for the drag & drop functionality
+            this.DragEnter += new DragEventHandler(Form_DragEnter);
+            this.DragDrop += new DragEventHandler(Form_DragDrop);
+        }
+
+        // This event occurs when the user drags over the form with 
+        // the mouse during a drag drop operation 
+        void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            // Check if the Dataformat of the data can be accepted
+            // (we only accept file drops from Explorer, etc.)
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy; // Okay
+            else
+                e.Effect = DragDropEffects.None; // Unknown data, ignore it
+
+        }
+
+        // Occurs when the user releases the mouse over the drop target 
+        void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            // clear the label
+            label1.Text = String.Empty;
+            // Extract the data from the DataObject-Container into a string list
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // Do something with the data...
+
+            // For example add all files into a simple label control:
+            foreach (string File in FileList)
+                this.label1.Text += File + "\n";
+        }
+
+
     }
-}
+    }
+
