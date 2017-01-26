4cac94e496a765df750a3e8604a0944e9a8394ca
Kieran Hill <dilligaf1979@gmail.com>
2017-01-26 20:45:38 +0000

    added more items

--- DragnDrop Renamer/Form1.Designer.cs
+++ DragnDrop Renamer/Form1.Designer.cs
@@ -31,6 +31,17 @@
             this.components = new System.ComponentModel.Container();
             this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.salesorder1 = new DevExpress.XtraEditors.TextEdit();
+            this.contractnumber1 = new DevExpress.XtraEditors.TextEdit();
+            this.filetype1 = new DevExpress.XtraEditors.TextEdit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkckmn1 = new DevExpress.XtraEditors.CheckButton();
+            this.salesorderlabel = new System.Windows.Forms.Label();
+            this.contractnumberlabel = new System.Windows.Forms.Label();
+            this.filetypelabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.salesorder1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contractnumber1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filetype1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // defaultLookAndFeel1
@@ -41,11 +52,90 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(370, 23);
+            this.label1.Location = new System.Drawing.Point(350, 69);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Old File Name";
+            // 
+            // salesorder1
+            // 
+            this.salesorder1.EditValue = "";
+            this.salesorder1.Location = new System.Drawing.Point(47, 66);
+            this.salesorder1.Name = "salesorder1";
+            this.salesorder1.Properties.Appearance.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.salesorder1.Properties.Appearance.Options.UseForeColor = true;
+            this.salesorder1.Size = new System.Drawing.Size(100, 20);
+            this.salesorder1.TabIndex = 1;
+            // 
+            // contractnumber1
+            // 
+            this.contractnumber1.EditValue = "";
+            this.contractnumber1.Location = new System.Drawing.Point(47, 119);
+            this.contractnumber1.Name = "contractnumber1";
+            this.contractnumber1.Properties.Appearance.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.contractnumber1.Properties.Appearance.Options.UseForeColor = true;
+            this.contractnumber1.Size = new System.Drawing.Size(100, 20);
+            this.contractnumber1.TabIndex = 2;
+            // 
+            // filetype1
+            // 
+            this.filetype1.EditValue = "";
+            this.filetype1.Location = new System.Drawing.Point(47, 174);
+            this.filetype1.Name = "filetype1";
+            this.filetype1.Properties.Appearance.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.filetype1.Properties.Appearance.Options.UseForeColor = true;
+            this.filetype1.Size = new System.Drawing.Size(100, 20);
+            this.filetype1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(350, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New File name";
+            // 
+            // checkckmn1
+            // 
+            this.checkckmn1.Location = new System.Drawing.Point(209, 177);
+            this.checkckmn1.LookAndFeel.SkinName = "Visual Studio 2013 Dark";
+            this.checkckmn1.Name = "checkckmn1";
+            this.checkckmn1.Size = new System.Drawing.Size(75, 23);
+            this.checkckmn1.TabIndex = 5;
+            this.checkckmn1.Text = "CK On";
+            // 
+            // salesorderlabel
+            // 
+            this.salesorderlabel.AutoSize = true;
+            this.salesorderlabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.salesorderlabel.Location = new System.Drawing.Point(47, 47);
+            this.salesorderlabel.Name = "salesorderlabel";
+            this.salesorderlabel.Size = new System.Drawing.Size(63, 13);
+            this.salesorderlabel.TabIndex = 6;
+            this.salesorderlabel.Text = "Sales Order";
+            // 
+            // contractnumberlabel
+            // 
+            this.contractnumberlabel.AutoSize = true;
+            this.contractnumberlabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.contractnumberlabel.Location = new System.Drawing.Point(50, 100);
+            this.contractnumberlabel.Name = "contractnumberlabel";
+            this.contractnumberlabel.Size = new System.Drawing.Size(89, 13);
+            this.contractnumberlabel.TabIndex = 7;
+            this.contractnumberlabel.Text = "Contract Number";
+            // 
+            // filetypelabel
+            // 
+            this.filetypelabel.AutoSize = true;
+            this.filetypelabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.filetypelabel.Location = new System.Drawing.Point(53, 155);
+            this.filetypelabel.Name = "filetypelabel";
+            this.filetypelabel.Size = new System.Drawing.Size(50, 13);
+            this.filetypelabel.TabIndex = 8;
+            this.filetypelabel.Text = "File Type";
             // 
             // Form1
             // 
@@ -53,11 +143,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(698, 431);
+            this.Controls.Add(this.filetypelabel);
+            this.Controls.Add(this.contractnumberlabel);
+            this.Controls.Add(this.salesorderlabel);
+            this.Controls.Add(this.checkckmn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filetype1);
+            this.Controls.Add(this.contractnumber1);
+            this.Controls.Add(this.salesorder1);
             this.Controls.Add(this.label1);
             this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
             this.Name = "Form1";
             this.Text = "DragnDrop Renamer";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.salesorder1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contractnumber1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filetype1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +168,14 @@
 
         private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
         private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TextEdit salesorder1;
+        private DevExpress.XtraEditors.TextEdit contractnumber1;
+        private DevExpress.XtraEditors.TextEdit filetype1;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.CheckButton checkckmn1;
+        private System.Windows.Forms.Label salesorderlabel;
+        private System.Windows.Forms.Label contractnumberlabel;
+        private System.Windows.Forms.Label filetypelabel;
     }
 }
 
